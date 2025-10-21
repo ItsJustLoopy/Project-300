e2c4e1ce2e273b5f96cf0f0ac7ffc9bc6e8b7856
SeanRiain <s00254331@atu.ie>
2025-10-21 17:36:42 +0100

    Basic Level Select Implemented

--- Assets/Scripts/UI Scripts/MenuFunctionality.cs
+++ Assets/Scripts/UI Scripts/MenuFunctionality.cs
@@ -5,13 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MenuFunctionality : MonoBehaviour
 {
-    int sceneBuildIndex; 
+    int sceneBuildIndex;
     //This should allow the main play button to hold any level id therefore enabling the player to continue at whatever their
-    //last played level was. If this works correctly as is, it should have the same function as a new game option when the player first starts.
+    //last played level was. If this works correctly as is,
+    //it should have the same function as a new game option when the player first starts.
     public void StartContinue()
 
     {
         SceneManager.LoadSceneAsync(sceneBuildIndex);
+    }
+
+    //As all levels (scenes) are formatted as "Level [number]", we can call a method like this upon clicking a level button.
+    //Having an int in the method parameters means that each time this method is assigned in the editor, it will ask for a number.
+    //We hardcode that number through the editor, making it the same as the levels number in the scene name,
+    //then once its recorded, assign a string variable representing
+    //the unique ID of the level (its full scene name) to be a combination of "Level " and the number.
+    //Once we have this, we can call the loadscene method to open a scene with with the same name as our generated LevelID
+    //which will be the correct level/scene every time.
+    public void LevelLoad(int LevelNumber)
+    {
+        string LevelID = $"Level {LevelNumber}";
+        SceneManager.LoadScene(LevelID);
     }
 
     public void ExitApp()
