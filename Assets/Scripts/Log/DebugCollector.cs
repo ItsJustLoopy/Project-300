4c6f2c8806ca7bf764062a5e53fe44e865750425
DiarmuidCoyne <s00250978@atu.ie>
2025-12-04 12:39:49 +0000

    created debug log text file saver(working on fixing debugs before logger initializes)

---
+++ Assets/Scripts/Log/DebugCollector.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[InitializeOnLoad]
+public static class DebugCollector 
+{
+    private static string logFilePath;
+
+    static DebugCollector()
+    {
+        //set file path and folder if missing
+        string folder = Path.Combine(Application.dataPath, "Scripts/Log");
+        Directory.CreateDirectory(folder);
+        logFilePath = Path.Combine(folder, "ConsoleLog.txt");
+        //clearing out previous logs
+
+        File.WriteAllText(logFilePath, "--New Session-- \n");
+        //debug for finding where log is located
+        Debug.Log($"File saved to {logFilePath} ");
+        Application.logMessageReceived += HandleEditorLog;
+
+    }
+
+    private static void HandleEditorLog(string LogString, string stackTrace, LogType type)
+    {
+        string entry = $"[{System.DateTime.Now:HH:mm:ss}] [{type}] {LogString}\n";
+        //includes stack trace for errors and exceptions
+        if (type == LogType.Error || type == LogType.Exception)
+        {
+            entry += stackTrace + "\n";
+        }
+        File.AppendAllText(logFilePath, entry);
+    }
+}
